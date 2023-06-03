598b481fc049e97dcfadf22fd9c3ccf05d55f0f9
Dulltristan <WCGUltimateGamerlol@gmail.com>
2023-06-03 15:30:37 -0500

    Update Card_Database.gd
    added more card types and startercards.

--- Assets/Cards/Card_Database.gd
+++ Assets/Cards/Card_Database.gd
@@ -1,3 +1,18 @@
-#BookInfo = ["type","theme","rarity","name",]
-#TrinketInfo = ["type","theme","rarity","name","passiveType"]
-#HatInfo = ["type","theme","rarity","name"]
+#BookInfo = ["type","theme","rarity","name","passiveType","passiveValue","activeType","activeValue"]
+#TrinketInfo = ["type","theme","rarity","name","passiveType","passiveValue"]
+#HatInfo = ["type","theme","rarity","name","passiveType","passiveValue","armorValue"]
+#WeaponInfo ["type","theme","rarity","name","passiveType","passiveValue","damageType","damageValue"]
+enum {topHatOld,cricketBat,rosaryOld,tomeOld}
+
+const DATA = 
+{
+	topHatOld :
+		["hat","general","starter","Old Top Hat","NONE",0,1]
+	cricketBat :
+		["weapon","general","starter","Child's Cricket Bat","NONE",0,"general","2"]
+	rosaryOld :
+		["trinket","biblical","starter","Broken Rosary","","passiveValue"]
+	tomeOld :
+		["book","general","starter","name","passiveType","passiveValue","activeType","activeValue"]
+		
+}
