9e2f880401f3ca5867fa74802f7a7240ac31741e
Lizzy <55554146+lizzyLoki@users.noreply.github.com>
2023-06-03 17:11:19 -0500

    update the hat

--- character_modifications.gd
+++ character_modifications.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var man := $PotentialMan2ItemSlotHighlight as Sprite2D
 @onready var tophat := $JustHat as Sprite2D
+@onready var manWithHat := $PotentialMan2TopHat as Sprite2D
 
 # check to see if character's hands are holding anything
 var handLFill = false
@@ -10,16 +11,35 @@ var neckFill = false
 var hatFill = false
 
 func _on_hat_dropped():
-	hat = true
-	
+	if hatFill == true:
+		slot_error()
+	hatFill = true
+	queue_free()
+	return -1
+
 func _on_left_dropped():
+	if handLFill == true:
+		slot_error()
 	handLFill = true
-	
+	queue_free()
+	return -1
+
 func _on_right_dropped():
+	if handRFill == true:
+		slot_error()
 	handRFill = true
-	
+	queue_free()
+	return -1
+
 func _on_neck_dropped():
+	if neckFill == true:
+		slot_error()
 	neckFill = true
+	queue_free()
+	return -1
+
+func slot_error():
+	print("You may only fill the slot with one item at a time")
 
 
 # Called when the node enters the scene tree for the first time.
