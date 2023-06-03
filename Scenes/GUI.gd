2ecafe9aafb2cc84dbec231b4f4cbc52dce6cdba
Scott Kersh <tharmoth@gmail.com>
2023-06-03 17:44:33 -0500

    fix merge issues

--- Scenes/GUI.gd
+++ Scenes/GUI.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var simultaneous_scene = preload("res://Scenes/Autobattler.tscn").instantiate()
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,8 +11,7 @@ func _process(delta):
 
 
 func _on_button_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Autobattler.tscn")
-
+	get_tree().change_scene_to_file("res://Scenes/AutoBattler/Autobattler.tscn")
 
 func _on_button_3_quit_pressed():
 	get_tree().quit()
