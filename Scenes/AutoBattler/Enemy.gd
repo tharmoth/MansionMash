e6152b5ac5c7c359f1f931d2d4bbf245079da109
AnonymousFood <56987492+AnonymousFood@users.noreply.github.com>
2023-06-04 04:42:23 -0500

    Added basic deck functionality

--- Scenes/AutoBattler/Enemy.gd
+++ Scenes/AutoBattler/Enemy.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal dead
 var targets: Array[Node2D]
 
 const SPEED = 300.0
@@ -9,6 +10,8 @@ const SPEED = 300.0
 @onready var health = 100
 @onready var health_bar := $HealthBar as ProgressBar
 @onready var hit_timer := $HitTimer as Timer
+@onready var blood_particles := $BloodParticles2D as GPUParticles2D
+@onready var sprite := $AnimatedSprite2D as AnimatedSprite2D
 
 func _on_area_entered(area):
 	if not area in targets:
@@ -29,4 +32,14 @@ func subtract_health(value: int) -> void:
 	health = max(0, health - value)
 	health_bar.value = health
 	if(health == 0):
+		# Blood!
+		blood_particles.emitting = true
+		# Disable damaging hitbox
+		$CollisionShape2D.set_deferred("disabled", true)
+		# Loot!
+
+		var tween = get_tree().create_tween()
+		tween.tween_property(sprite, "scale", Vector2(1, 0), 2)
+		await get_tree().create_timer(2).timeout
+		dead.emit()
 		queue_free()
