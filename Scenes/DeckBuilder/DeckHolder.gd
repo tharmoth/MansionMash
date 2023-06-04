8c81723e7d9442b54e71745177e94d67916c6df4
Scott Kersh <tharmoth@gmail.com>
2023-06-03 22:12:29 -0500

    improved deck building gui

--- Scenes/DeckBuilder/DeckHolder.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body):
-	if body.has_method("in_deck"):
-		body.in_deck(true)
-
-func _on_body_exited(body):
-	if body.has_method("in_deck"):
-		body.in_deck(false)
--- Scenes/DeckBuilder/card_producer.gd
+++ Scenes/DeckBuilder/card_producer.gd
@@ -3,12 +3,15 @@ extends Area2D
 @export var DragAndDropCard: PackedScene
 var top_of_deck = null
 
+func _ready():
+	body_exited.connect(_on_body_exited)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if top_of_deck == null:
 		top_of_deck = DragAndDropCard.instantiate()
-		top_of_deck.position = position
-		get_node("/root/DeckBuilder").add_child(top_of_deck)
+		top_of_deck.position = global_position
+		get_node("/root/GUI").add_child(top_of_deck)
 
 func _on_body_exited(body):
 	if body == top_of_deck:
