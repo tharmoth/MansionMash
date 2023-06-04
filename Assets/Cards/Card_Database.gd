e1a28261b8da7e8632ed8f8d47eb95a9189081f8
Dulltristan <WCGUltimateGamerlol@gmail.com>
2023-06-04 03:20:31 -0500

    Adding more cards

--- Assets/Cards/Card_Database.gd
+++ Assets/Cards/Card_Database.gd
@@ -4,7 +4,7 @@
 #WeaponInfo ["type","theme","rarity","name","passiveType","passiveValue","damageType","damageValue"]
 
 #Grit is Sanity armor
-enum {topHatOld,cricketBat,rosaryOld,tomeOld}
+enum {topHatOld,cricketBat,rosaryOld,tomeOld,lesserKey,necronomicon}
 
 const DATA = {
 	"topHatOld" :
@@ -14,5 +14,9 @@ const DATA = {
 	"rosaryOld" :
 		["trinket","biblical","starter","Broken Rosary","Holy Armor","1"],
 	"tomeOld" :
-		["book","general","starter","Grandpa's Journal","Grit","2","NONE",0]
+		["book","general","starter","Grandpa's Journal","Grit","2","NONE",0],
+	"lesserKey" :
+		["book","biblical","occultic","The Lesser Key of Solomon","Grit","-5","Accuracy", 10], #Accuracy plus 10%
+	"necronomicon" :
+		["book","eldritch","occultic","The Necronomicon","Grit","-10","Damage Multiplier",4]
 }
