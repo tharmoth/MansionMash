fd8bb6170dbad9e88614208406f8164258aebb46
Scott Kersh <tharmoth@gmail.com>
2023-06-03 19:49:23 -0500

    added deck builder to main menu

--- Scenes/GUI.gd
+++ Scenes/GUI.gd
@@ -17,3 +17,7 @@ func _on_button_play_pressed():
 
 func _on_button_3_quit_pressed():
 	get_tree().quit()
+
+
+func _on_button_2_settings_pressed():
+	get_tree().change_scene_to_file("res://Scenes/DeckBuilder/deck_builder.tscn")
