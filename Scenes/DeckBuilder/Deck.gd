0bc59b44304f544eb241ee1d140a33eca6f77be4
Scott Kersh <tharmoth@gmail.com>
2023-06-03 20:49:38 -0500

    deck updates

--- Scenes/DeckBuilder/Deck.gd
+++ Scenes/DeckBuilder/Deck.gd
@@ -3,11 +3,6 @@ extends Area2D
 @export var DragAndDropCard: PackedScene
 var top_of_deck = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if top_of_deck == null:
--- Scenes/DeckBuilder/DeckHolder.gd
+++ Scenes/DeckBuilder/DeckHolder.gd
@@ -1,16 +1,9 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_body_entered(body):
 	if body.has_method("in_deck"):
 		body.in_deck(true)
 
+func _on_body_exited(body):
+	if body.has_method("in_deck"):
+		body.in_deck(false)
