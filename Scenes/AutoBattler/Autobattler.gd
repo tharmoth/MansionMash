471da12956d65ab3e0fd67aeae4ea224661c9aa0
AnonymousFood <56987492+AnonymousFood@users.noreply.github.com>
2023-06-03 23:34:44 -0500

    Added fancy things

--- Scenes/AutoBattler/Autobattler.gd
+++ Scenes/AutoBattler/Autobattler.gd
@@ -1,5 +1,19 @@
 extends Node2D
 
+
+var enemy = preload("res://Scenes/AutoBattler/enemy.tscn")
+
+func _ready() -> void:
+#	for i in 1000:
+#		set_pattern(0, Vector2i(i, 0), tile_set.get_pattern(0))
+	
+	for n in range(1000,100000,1000):
+		var my_enemy = enemy.instantiate()
+		add_child(my_enemy)
+		my_enemy.position.x = n
+		my_enemy.position.y = 370
+
+
 func game_over():
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
 
@@ -8,3 +22,4 @@ func _on_return_button_pressed():
 
 func _on_player_dead():
 	game_over()
+
