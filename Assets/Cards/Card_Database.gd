411aa994093dacdd6a3643d1e68b5534fe9fc90b
Dulltristan <WCGUltimateGamerlol@gmail.com>
2023-06-03 22:48:35 -0500

    Completed minimal card builder

--- Assets/Cards/Card_Database.gd
+++ Assets/Cards/Card_Database.gd
@@ -1,14 +1,14 @@
 #BookInfo = ["type","theme","rarity","name","passiveType","passiveValue","activeType","activeValue"]
 #TrinketInfo = ["type","theme","rarity","name","passiveType","passiveValue"]
-#HatInfo = ["type","theme","rarity","name","passiveType","passiveValue","armorValue"]
+#HatInfo = ["type","theme","rarity","name","passiveType","passiveValue","armor","armorValue"]
 #WeaponInfo ["type","theme","rarity","name","passiveType","passiveValue","damageType","damageValue"]
 enum {topHatOld,cricketBat,rosaryOld,tomeOld}
 
 const DATA = {
 	topHatOld :
-		["hat","general","starter","Old Top Hat","NONE",0,1],
+		["hat","general","starter","Old Top Hat","NONE",0,"Armor",1],
 	cricketBat :
-		["weapon","general","starter","Child's Cricket Bat","NONE",0,"general","2"],
+		["weapon","general","starter","Child's Cricket Bat","NONE",0,"general",2],
 	rosaryOld :
 		["trinket","biblical","starter","Broken Rosary","","passiveValue"],
 	tomeOld :
